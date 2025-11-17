fb7a9c572754b89c17f1642a5ced22132c328844
Riky Raj <rajriky28@gmail.com>
2025-11-17 08:30:59 +0000

    add blog link in footer

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -16,6 +16,7 @@ export function Footer() {
     Company: [
       { name: 'About', href: '/about' },
       { name: 'Vision', href: '/vision' },
+      { name: 'Blog', href: '/blog' },
       { name: 'Careers', href: '/careers' },
       { name: 'Contact', href: '/contact' },
     ],
